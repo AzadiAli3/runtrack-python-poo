33bb20f40ef95b2f1d100ae526b3c23571015670
Wiki OwSki <azad.ali@laplateforme.io>
2025-02-04 09:39:28 +0100

    Ajout du job 3

---
+++ Job_03.py
@@ -0,0 +1,18 @@
+class Operation:
+    def __init__(self, nombre1=0, nombre2=0):
+        self.nombre1 = nombre1
+        self.nombre2 = nombre2
+
+    def addition(self):
+        resultat = self.nombre1 = self.nombre2
+        print("Le resultat de l'addition est {resultat}")
+
+operation1 = Operation()
+print(operation1)
+
+operation2 = Operation(5, 10)
+print(operation2)
+
+operation2.addition()
+
+        
